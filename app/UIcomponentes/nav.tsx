783e3940246495b0351be9f681a22f23a030c309
Anas Alaoui <96668023+Kngdmn34@users.noreply.github.com>
2023-12-25 23:04:02 +0100

    Update nav.tsx

--- app/UIcomponentes/nav.tsx
+++ app/UIcomponentes/nav.tsx
@@ -4,7 +4,7 @@ import Logo from '@/public/images/Logo_Royal_Air_Maroc_2020.png'
 
 const nav = () => {
     return (
-        <nav className='fixed  py-5 backdrop-blur-xl w-full z-20  top-0 border-b shadow-md lg:shadow-lg shadow-red-800'>
+        <nav className='fixed  py-5 backdrop-blur-xl w-full z-20  top-0 border-b shadow-md shadow-red-700 lg:shadow-lg shadow-red-800'>
             <div className='relative w-full flex justify-center items-center'>
                 <Image className='absolute p-2  shadow-md shadow-green-800 bg-gray-50 -bottom-10 ' src={Logo} alt='logo' height={100} width={100} />
 
